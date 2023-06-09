35d47b8e9e11f04aabf4bbc33af39cfa5c9ce114
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 22:19:43 +0900

    [Finish]コンストラクタの作成

--- lesson2/index.php
+++ lesson2/index.php
@@ -2,18 +2,24 @@
 class Menu {
   public $name;
 
+  // コンストラクタの定義
+  public function __construct() {
+    echo "1つのメニューが作られました";
+  }
+
+
   public function hello() {
-    // echo "私はMenuクラスのインスタンスです";
-    // 私は**です とechoする
     echo "私は{$this->name}です";
   }
 
 }
 // Menuクラスのインスタンスの作成、各変数へ
 $curry = new Menu();
+echo '<br>';
 $pasta = new Menu();
 // 各変数のnameプロパティに対して値をセット
 $curry->name = "CURRY";
+echo '<br>';
 $pasta->name = "PASTA";
 // 各インスタンスに対してhelloメソッドを呼び出し
 $curry->hello();
