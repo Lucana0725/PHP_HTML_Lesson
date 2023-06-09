d46b64882b172857a7b29c9fd8a0b6ed34de5083
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 22:36:34 +0900

    [Finish]HTMLとCSSを拝借して準備

--- lesson2/index.php
+++ lesson2/index.php
@@ -24,6 +24,24 @@ $pasta = new Menu('PASTA');
 // $pasta->hello();
 ?>
 
-<!-- 各<p>の中で各インスタンスのnameプロパティを呼び出す -->
-<p><?php echo $curry->name; ?></p>
-<p><?php echo $pasta->name; ?></p>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      <!-- 配列$menusの要素を変数$menuとするforeach文を書いてください -->
+      
+      
+    </div>
+  </div>
+</body>
+</html>
