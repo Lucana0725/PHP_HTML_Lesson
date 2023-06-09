a06f91b5bf49b1dacf1e21ef86d6121d1e807ba3
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 22:17:15 +0900

    [Finish]thisを使ってプロパティへアクセス

--- lesson2/index.php
+++ lesson2/index.php
@@ -1,12 +1,11 @@
 <?php
-// Menuクラスの作成
 class Menu {
-  // nameプロパティを作成
   public $name;
 
-  // helloメソッドを定義
   public function hello() {
-    echo "私はMenuクラスのインスタンスです";
+    // echo "私はMenuクラスのインスタンスです";
+    // 私は**です とechoする
+    echo "私は{$this->name}です";
   }
 
 }
@@ -16,7 +15,6 @@ $pasta = new Menu();
 // 各変数のnameプロパティに対して値をセット
 $curry->name = "CURRY";
 $pasta->name = "PASTA";
-
 // 各インスタンスに対してhelloメソッドを呼び出し
 $curry->hello();
 echo '<br>';
