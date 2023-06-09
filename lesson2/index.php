1de0909c3c99b0d82cc5aeef35f874c3d7acad4d
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 21:24:32 +0900

    [Finish]nameプロパティを定義、各変数のnameプロパティに値をセットし、表示してみる

--- lesson2/index.php
+++ lesson2/index.php
@@ -1,10 +1,21 @@
 <?php
 // Menuクラスの作成
 class Menu {
+  // nameプロパティを作成
+  public $name;
 
 }
 // Menuクラスのインスタンスの作成、各変数へ
 $curry = new Menu();
 $pasta = new Menu();
+
+// 各変数のnameプロパティに対して値をセット
+$curry->name = "CURRY";
+$pasta->name = "PASTA";
+
+// 各nameプロパティをechoする
+echo $curry->name;
+echo '<br>';
+echo $pasta->name;
 
 ?>
