cc99ffffc4bc8496154fdf36c8678f2aca93f6ab
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 21:29:35 +0900

    [Finish]メソッドの作成と呼び出し

--- lesson2/index.php
+++ lesson2/index.php
@@ -4,18 +4,21 @@ class Menu {
   // nameプロパティを作成
   public $name;
 
+  // helloメソッドを定義
+  public function hello() {
+    echo "私はMenuクラスのインスタンスです";
+  }
+
 }
 // Menuクラスのインスタンスの作成、各変数へ
 $curry = new Menu();
 $pasta = new Menu();
-
 // 各変数のnameプロパティに対して値をセット
 $curry->name = "CURRY";
 $pasta->name = "PASTA";
 
-// 各nameプロパティをechoする
-echo $curry->name;
+// 各インスタンスに対してhelloメソッドを呼び出し
+$curry->hello();
 echo '<br>';
-echo $pasta->name;
-
+$pasta->hello();
 ?>
