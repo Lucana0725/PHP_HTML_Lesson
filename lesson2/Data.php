53904fefd840a391c406302ed3385f7a8560a3fa
Lucana <k.ark.scarlet72@gmail.com>
2023-06-13 19:22:57 +0900

    [Finish]confirm.phpを拝借、orderCountをforeach内に新たに定義、[->getName()]としてmenuのnameが渡されるように。

--- lesson2/Data.php
+++ lesson2/Data.php
@@ -7,8 +7,6 @@ $coffee = new Menu('COFFEE', 500, "https://s3-ap-northeast-1.amazonaws.com/proga
 $curry = new Menu('CURRY', 900, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png");
 $pasta = new Menu('PASTA', 1200, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png");
 
-// juiceに対してsetOrderCountの引数を2にして呼び出し。
-$juice->setOrderCount(2);
 
 // 配列の中に上記の4つのインスタンスを順に入れて、変数$menusに代入
 $menus = array($juice, $coffee, $curry, $pasta);
