c66aa8e16dd393d35d80356c1b6222b7017ae556
Lucana <k.ark.scarlet72@gmail.com>
2023-06-13 22:56:02 +0900

    [Finish]注文の合計金額を表示するため、foreach内でtotalPaymentの定義と計算

--- lesson2/confirm.php
+++ lesson2/confirm.php
@@ -14,12 +14,17 @@ require_once('data.php');
 <body>
   <div class="order-wrapper">
     <h2>注文内容確認</h2>
+    <!-- 支払い金額の表示目的, $totalPayment -->
+    <?php $totalPayment = 0; ?>
+
     <?php foreach ($menus as $menu): ?>
       <!-- 変数$orderCountを定義、$_POSTで受け取った値を代入 -->
       <?php
         $orderCount = $_POST[$menu->getName()];
         // $menuに注文個数をセットする
         $menu->setOrderCount($orderCount);
+        // $totalPaymentに、$menuのgetTotalPrice()で得た値を足し合わせていく
+        $totalPayment += $menu->getTotalPrice();
       ?>
       <p class="order-amount">
         <!-- ここに、$menuのゲッターを用いてnameプロパティを表示 -->
@@ -31,6 +36,7 @@ require_once('data.php');
       </p>
       <p class="order-price"><?php echo $menu->getTotalPrice(); ?> 円</p>
     <?php endforeach ?>
+    <h3>合計金額：<?php echo $totalPayment; ?> 円</h3>
   </div>
 </body>
 </html>
