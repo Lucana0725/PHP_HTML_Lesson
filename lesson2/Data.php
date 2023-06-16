1fa779564e2b22879c38d9566a4e9aedfcd4721d
Lucana <k.ark.scarlet72@gmail.com>
2023-06-16 22:04:59 +0900

    [Finish]レビューとレビュワーを紐付けるためにuserNameプロパティを定義、紐づいているかを調べるgetUserメソッドを定義

--- lesson2/Data.php
+++ lesson2/Data.php
@@ -4,15 +4,15 @@ require_once('Drink.php');
 require_once('Food.php');
 require_once('Review.php');
 
+
 // 各々price, imageの値をセット
 $juice = new Drink('JUICE', 600, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png", 'アイス');
 $coffee = new Drink('COFFEE', 500, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png", 'ホット');
 $curry = new Food('CURRY', 900, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png", 3);
 $pasta = new Food('PASTA', 1200, "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png", 1);
 
-
-// 配列の中に上記の4つのインスタンスを順に入れて、変数$menusに代入
 $menus = array($juice, $coffee, $curry, $pasta);
+
 
 // User関連
 $user1 = new User('suzuki', 'male');
@@ -22,16 +22,15 @@ $users = array($user1, $user2);
 
 
 // レビュー用のreviewインスタンス
-$review1 = new Review($juice->getName(), '果肉たっぷりのオレンジジュースです！');
-$review2 = new Review($curry->getName(), '具がゴロゴロしていてとてもおいしいです');
-$review3 = new Review($coffee->getName(), '香りがいいです');
-$review4 = new Review($pasta->getName(), 'ソースが絶品です。また食べたい。');
-$review5 = new Review($juice->getName(), '普通のジュース');
-$review6 = new Review($curry->getName(), '値段の割においしいカレーだと思いました');
-$review7 = new Review($coffee->getName(), '苦味がちょうどよくて、おすすめです');
-$review8 = new Review($pasta->getName(), '具材にこだわりを感じました。');
+$review1 = new Review($juice->getName(), $user1->getName(), '果肉たっぷりのオレンジジュースです！');
+$review2 = new Review($curry->getName(), $user1->getName(), '具がゴロゴロしていてとてもおいしいです');
+$review3 = new Review($coffee->getName(), $user1->getName(), '香りがいいです');
+$review4 = new Review($pasta->getName(), $user1->getName(), 'ソースが絶品です。また食べたい。');
+$review5 = new Review($juice->getName(), $user2->getName(), '普通のジュース');
+$review6 = new Review($curry->getName(), $user2->getName(), '値段の割においしいカレーだと思いました');
+$review7 = new Review($coffee->getName(), $user2->getName(), '苦味がちょうどよくて、おすすめです');
+$review8 = new Review($pasta->getName(), $user2->getName(), '具材にこだわりを感じました。');
 
-// $reviewが格納された配列$reviews
 $reviews = array($review1, $review2, $review3, $review4, $review5, $review6, $review7, $review8);
 
 ?>
--- lesson2/Review.php
+++ lesson2/Review.php
@@ -2,11 +2,13 @@
 
 class Review {
   private $menuName;
+  private $userName;  // レビューとレビューした人の情報を紐付けるプロパティ
   private $body;
 
   // コンストラクタ
-  public function __construct($menuName, $body) {
+  public function __construct($menuName, $userName, $body) {
     $this->menuName = $menuName;
+    $this->userName = $userName;
     $this->body = $body;
   }
 
@@ -17,6 +19,16 @@ class Review {
 
   public function getBody() {
     return $this->body;
+  }
+
+
+  // レビューとレビュワーを紐付けるメソッド
+  public function getUser($users) {
+    foreach ($users as $user) {
+      if ($user->getName == $this->userName) {  // Userのユーザーネーム($name)とReviewのユーザーネーム($userName)が一致しているかどうか
+        return $user;  // 一致していれば、$userを返す
+      }
+    }
   }
 
 }
