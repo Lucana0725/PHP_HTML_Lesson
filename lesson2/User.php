226503e86d1b28b347ce3f83d45ada8112eec159
Lucana <k.ark.scarlet72@gmail.com>
2023-06-18 16:37:14 +0900

    [Finish]ユーザー識別のためにUserにidを付与、表示できるよう設定

--- lesson2/User.php
+++ lesson2/User.php
@@ -1,16 +1,24 @@
 <?php
 
 class User {
+  private $id;  // ユーザーごとにidをもたせることで同姓同名のユーザーでも識別できるようにする
   private $name;
   private $gender;
+  private static $count;  // $countをクラスプロパティ(static)にする
 
   // コンストラクタ
   public function __construct($name, $gender) {
     $this->name = $name;
     $this->gender = $gender;
+
+    self::$count++;  // コンストラクタがはたらく度に$countを+1する(インスタンスが生成されるたびにidが1, 2, ...となる。)
+    $this->id = self::$count;  // self::$count++;で値を増やしたidを$idがもつようにする。
   }
 
   // ゲッター
+  public function getId() {
+    return $this->id;
+  }
   public function getName() {
     return $this->name;
   }
