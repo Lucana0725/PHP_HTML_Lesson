e1ae1ad40828cbb7441de8c16f4dbfee08938757
Lucana <k.ark.scarlet72@gmail.com>
2023-06-18 16:49:07 +0900

    [Finish]レビューとレビュワーの紐づけをユーザー名からユーザーIDに変更。

--- lesson2/Review.php
+++ lesson2/Review.php
@@ -2,13 +2,14 @@
 
 class Review {
   private $menuName;
-  private $userName;  // レビューとレビューした人の情報を紐付けるプロパティ
+  // private $userName;  // レビューとレビューした人の情報を紐付けるプロパティ
+  private $userId;  // レビューとレビュワーの情報を紐付けるプロパティ。$userNameだと同姓同名のユーザーを識別できないので。
   private $body;
 
   // コンストラクタ
-  public function __construct($menuName, $userName, $body) {
+  public function __construct($menuName, $userId, $body) {  // コンストラクタもuserName=>userIdに変更。
     $this->menuName = $menuName;
-    $this->userName = $userName;
+    $this->userId = $userId;
     $this->body = $body;
   }
 
@@ -25,7 +26,8 @@ class Review {
   // レビューとレビュワーを紐付けるメソッド
   public function getUser($users) {
     foreach ($users as $user) {
-      if ($user->getName() == $this->userName) {  // Userのユーザーネーム($name)とReviewのユーザーネーム($userName)が一致しているかどうか
+      // if ($user->getName() == $this->userName) {  // Userのユーザーネーム($name)とReviewのユーザーネーム($userName)が一致しているかどうか
+      if ($user->getId() == $this->userId) {  // UserのユーザーID($user->getId())とReviewのユーザーID($this->userId)が一致しているかどうか(IDでの識別に変更)
         return $user;  // 一致していれば、$userを返す
       }
     }
